fff773a76755d3661a79cb17d367adc7b4f7fd5b
rollingcat <everydaycoding@gmail.com>
2019-09-04 22:31:35 +0900

    backtracking - string 1 - success

--- backtracking/src/user.cpp
+++ backtracking/src/user.cpp
@@ -8,10 +8,13 @@ extern void addString(char* input);
 void printPermutation(int index, char* input) {
   if (index == N) {
     addString(input);
-    printf("%s\n", input);
+    //printf("%s\n", input);
     return;
   }
 
   for (int i = index; i < N; i++) {
+	swap(input, index, i);
+	printPermutation(index + 1, input);
+	swap(input, index, i);
   }
 }
