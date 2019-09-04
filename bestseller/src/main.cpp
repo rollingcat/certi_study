b8bbf3c50f207d7a35e3119d547b7a8ecafbe572
rollingcat <everydaycoding@gmail.com>
2019-09-04 11:06:08 +0900

    bestseller - successs

--- bestseller/src/main.cpp
+++ bestseller/src/main.cpp
@@ -6,8 +6,8 @@
 
 static int seed = 18043000;
 static int pseudo_rand(void) {
-	seed = seed * 214013 + 2531011;
-	return (seed >> 16) & 0x7FFF;
+  seed = seed * 214013 + 2531011;
+  return (seed >> 16) & 0x7FFF;
 }
 
 static int numList[MAX_NUM];
@@ -16,32 +16,32 @@ extern void sell(int item_id, int num_sell);
 extern void best_sellers(int num, int idList[MAX_NUM]);
 
 int main() {
-	time_t start = clock();
+  time_t start = clock();
 
-	freopen("sample_input.txt", "r", stdin);
-	for (int tc = 0; tc <= 100; tc++)
-		for (int t = 1; t <= 10; t++) {
-			init();
+  freopen("sample_input.txt", "r", stdin);
+  for (int tc = 0; tc <= 100; tc++)
+  for (int t = 1; t <= 10; t++) {
+    init();
 
-			for (register int i = 0; i < 20000; i++) {
-				int item_id = pseudo_rand() % MAX_ITEMS + 1;
-				int num_sell = pseudo_rand();
-				sell(item_id, num_sell);
-			}
+    for (register int i = 0; i < 20000; i++) {
+      int item_id = pseudo_rand() % MAX_ITEMS + 1;
+      int num_sell = pseudo_rand();
+      sell(item_id, num_sell);
+    }
 
-			best_sellers(t, numList);
+    best_sellers(t, numList);
 
-			for (int i = 0; i < t; i++) {
-				int ans = 0;
-				scanf("%d", &ans);
-				if (ans != numList[i]) {
-					printf("FAIL\n");
-					return 0;
-				}
+    for (int i = 0; i < t; i++) {
+      int ans = 0;
+      scanf("%d", &ans);
+      if (ans != numList[i]) {
+        printf("FAIL\n");
+        return 0;
+      }
 
-			}
-		}
-	printf("PASS\n");
+    }
+  }
+  printf("PASS\n");
 
-	return 0;
+  return 0;
 }
--- bestseller/src/user.cpp
+++ bestseller/src/user.cpp
@@ -1,14 +1,89 @@
+
 #define MAX_NUM 10
 #define MAX_ITEMS 20000
 
-void init() {
+long long selling[MAX_ITEMS];
 
+struct Heap {
+  int data[MAX_NUM + 2];
+  int cnt;
+
+  void swap(int a, int b) {
+    if (a == b) return;
+    int c = data[a];
+    data[a] = data[b];
+    data[b] = c;
+  }
+
+  void push(int i) {
+    int idx = ++cnt;
+    data[idx] = i;
+    while (idx > 1) {
+      if (selling[data[idx]] < selling[data[idx >> 1]]) {
+        swap(idx, idx >> 1);
+        idx = idx >> 1;
+      } else break;
+    }
+  }
+
+  void pop() {
+    swap(cnt--, 1);
+    int idx = 1;
+    int next = 1;
+
+    while (true) {
+      if (idx << 1 <= cnt && selling[data[idx << 1]] < selling[data[next]]) {
+        next = idx << 1;
+      }
+      if ((idx << 1) + 1 <= cnt && selling[data[(idx << 1) + 1]] < selling[data[next]]) {
+        next = (idx << 1) + 1;
+      }
+
+      if (idx == next) break;
+
+      swap(idx, next);
+      idx = next;
+    }
+  }
+};
+
+Heap heap;
+
+void init() {
+  heap.cnt = 0;
+  for (int k = 0; k < MAX_ITEMS; ++k)
+    selling[k] = 0;
 }
 
 void sell(int item_id, int num_sell) {
-
+  selling[item_id - 1] += num_sell;
 }
 
 void best_sellers(int num, int idList[MAX_NUM]) {
+  int n = num;
+  while (n > 0) {
+    heap.push(--n);
+  }
 
+  for (int k = num; k < MAX_ITEMS; ++k) {
+    if (selling[k] < selling[heap.data[1]]) continue;
+
+    heap.pop();
+    heap.push(k);
+  }
+
+  for (int k = num - 1; k >= 0; --k) {
+    idList[k] = heap.data[1] + 1;
+    heap.pop();
+  }
+
+  for (int k = 1; k < num; ++k) {
+    if (selling[idList[k - 1] - 1] == selling[idList[k] - 1]) {
+      if (idList[k - 1] < idList[k]) {
+        int c = idList[k];
+        idList[k] = idList[k - 1];
+        idList[k - 1] = c;
+      }
+    }
+  }
 }
