5f0bb11c164f43357d09d51c3daed14cabb627f9
rollingcat <everdaycoding@gmail.com>
2019-09-06 15:53:07 +0900

    Production estimate

---
+++ production_estimate/src/user.cpp
@@ -0,0 +1,18 @@
+#define MAX_N    100
+#define MAX_TOOL 50
+
+void init(int N) {
+
+}
+
+void setupTool(int T, int stepNo[5000], int procTime[5000]) {
+
+}
+
+void addLot(int time, int number) {
+
+}
+
+int simulate(int time, int wip[MAX_N]) {
+  return 0;
+}
